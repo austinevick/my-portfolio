4fb868a640e122815c422660362b7dd1ac7323a0
Augustine Victor <augustinevickky@gmail.com>
2022-01-09 17:36:06 +0100

    modified:   lib/constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,5 @@
 const String description =
-    'I am a flutter mobile developer with focused on clean architecture and scalability in software design and application development.';
+    'I am a flutter mobile developer with focus on clean architecture and scalability in software design and application development.';
 
 const String aboutMe =
     '''I am a flutter mobile developer with over 2 years experience in software design and application development.\nI ventured into programming in 2018 while working in a vehicle tracking firm, Java was my favourite language, then I knew nothing about mobile development all I had was the passion for coding.\n\nNot until 2019 I decided to become a professional mobile engineer, I began with native android development but got so frustrated with the UI design and I began to seek for alternative. After many research I discovered flutter in a google codelab and I decided to buy the course and since then I came to love flutter not because it's perfect but it came at a point when I almost lost hope in my career.\n\nIn 2020, my passion for mobile development grew as well as my skills so I began to apply for jobs and internships I believed working with a team will help me become a better developer.\n\nGetting a job was hard not even internship, I discovered not everyone is ready to invest in you despite your zeal and potential so I forgot about getting a job and began to build personal projects, buy more online courses and develop my skill set, ventured into other technologies at a point things were tough trying to balance my present job and still find time to code and coupled with the fact that I was a part time student but that notwithstanding against all odds I knew what I wanted to become.\n\nChallenges don't break people they only make you tougher.\n\nFast forward to 2021 October, I decide to quit my job and become a full time mobile developer, I have worked remotely with different startups over the years and I learnt a lot. I created a platform where I teach young people flutter development.\nIt was a priviledge sharing my journey with you I hope you find it useful.''';
@@ -13,7 +13,7 @@ const String github = 'https://github.com/austinevick';
 const String linkedIn =
     'https://www.linkedin.com/in/augustine-victor-800b94172/';
 const String resumeUrl =
-    'https://docs.google.com/document/d/1MwGXPkkYgJYZMa_IJX5ZEnu2J1-CKwb-yIgANSPr1ZI/edit?usp=sharing';
+    'https://docs.google.com/document/d/1_I4NsZEgv-O5zf2wXousr6X0tiRod1T7J3Qx86dP4Gw/edit?usp=sharing';
 const String email = 'mailto:augustinevickky@gmail.com';
 
 const String profileUrl =
